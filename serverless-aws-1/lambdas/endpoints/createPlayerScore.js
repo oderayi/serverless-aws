d5668f2096bf11f4e1be358e8102bc09ef603ca1
Steven Oderayi <oderayi@gmail.com>
2023-04-18 19:30:56 +0100

    Add custom hooks

--- serverless-aws-1/lambdas/endpoints/createPlayerScore.js
+++ serverless-aws-1/lambdas/endpoints/createPlayerScore.js
@@ -1,21 +1,25 @@
+const yup = require('yup');
 const Responses = require('../common/API_responses');
 const Dynamo = require('../common/Dynamo');
-const { withHooks } = require('../common/hooks');
+const { hooksWithValidation } = require('../common/hooks');
 
 const tableName = process.env.tableName;
 
-const handler = async event => {
-    if (!event.pathParameters.ID) {
-        // failed without an ID
-        return Responses._400({ message: 'missing the ID from the path' });
-    }
+const bodySchema = yup.object().shape({
+    name: yup.string().required(),
+    score: yup.number().required(),
+});
 
+const pathSchema = yup.object().shape({
+    ID: yup.string().required(),
+});
+
+const handler = async event => {
     let ID = event.pathParameters.ID;
     const user = event.body;
     user.ID = ID;
 
     const newUser = await Dynamo.write(user, tableName);
-
 
     if (!newUser) {
         return Responses._400({ message: 'Failed to write user by ID' });
@@ -25,4 +29,4 @@ const handler = async event => {
 
 }
 
-exports.handler = withHooks(handler);
+exports.handler = hooksWithValidation({ bodySchema, pathSchema})(handler,);
